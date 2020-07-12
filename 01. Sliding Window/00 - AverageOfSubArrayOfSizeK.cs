33507179f32c1675ba036876b3ac226937506174
Ehimah Obuse <ehimahobuse@gmail.com>
2020-07-12 16:19:24 +0100

    problem description

--- 01. Sliding Window/00 - AverageOfSubArrayOfSizeK.cs
+++ 01. Sliding Window/00 - AverageOfSubArrayOfSizeK.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/**
+        Given an array, find the average of all contiguous subarrays of size ‘K’ in it.
+        Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
+**/
+using System;
 
 namespace gtci
 {
