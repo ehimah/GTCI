c6d67a24b3b29ff648fa26aef5cea999a40dba66
Ehimah Obuse <ehimahobuse@gmail.com>
2020-07-12 17:24:38 +0100

    Maximum Sum Subarray of Size K

---
+++ 01. Sliding Window/01 - MaxSumSubArrayOfSizeK.cs
@@ -0,0 +1,53 @@
+/**
+Given an array of positive numbers and a positive number ‘k’, find the maximum sum of any contiguous subarray of size ‘k’.
+
+Example 1:
+
+Input: [2, 1, 5, 1, 3, 2], k=3 
+Output: 9
+Explanation: Subarray with maximum sum is [5, 1, 3].
+
+Example 2:
+
+Input: [2, 3, 4, 1, 5], k=2 
+Output: 7
+Explanation: Subarray with maximum sum is [3, 4].
+**/
+
+using System;
+
+namespace gtci
+{
+    public class MaxSumSubArrayOfSizeK
+    {
+        public static int findMaxSumSubArray(int K, int[] arr)
+        {
+            int windowSum = 0;
+            int maximumSum = 0;
+            int windowStart = 0;
+            for (int windowEnd = 0; windowEnd < arr.Length; windowEnd++)
+            {
+                windowSum += arr[windowEnd];
+
+                if (windowEnd >= K - 1)
+                {
+                    maximumSum = Math.Max(maximumSum, windowSum);
+                    //remove first element of window from the window
+                    windowSum -= arr[windowStart];
+                    //slide the window
+                    windowStart++;
+                }
+            }
+            return maximumSum;
+        }
+
+        public static void Run()
+        {
+            int result1 = MaxSumSubArrayOfSizeK.findMaxSumSubArray(3, new int[] { 2, 1, 5, 1, 3, 2 });
+            int result2 = MaxSumSubArrayOfSizeK.findMaxSumSubArray(2, new int[] { 2, 3, 4, 1, 5 });
+            Console.WriteLine($"Max Sum of subarrays of size K=3: {result1}");
+            Console.WriteLine($"Max Sum of subarrays of size K=2: {result2}");
+        }
+    }
+
+}
