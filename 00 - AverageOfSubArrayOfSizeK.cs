b614d412dab8a179ffae2e100c00a01a34aba2cf
Ehimah Obuse <ehimahobuse@gmail.com>
2020-07-12 16:16:12 +0100

    Basic sliding window - AverageOfSubArrayOfSizeK

---
+++ 00 - AverageOfSubArrayOfSizeK.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace gtci
+{
+    class AverageOfSubArrayOfSizeK
+    {
+        public static double[] findAverages(int K, int[] arr)
+        {
+            double[] result = new double[arr.Length - K + 1];
+            int windowStart = 0;
+            double windowSum = 0;
+            for (int windowEnd = 0; windowEnd < arr.Length; windowEnd++)
+            {
+                windowSum += arr[windowEnd];
+                if (windowEnd >= K - 1)
+                {
+                    result[windowStart] = windowSum / K;
+                    windowSum -= arr[windowStart];
+                    windowStart++;
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            double[] result = AverageOfSubArrayOfSizeK.findAverages(5, new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 });
+            Console.WriteLine($"Averages of subarrays of size K: { string.Join(',', result) }");
+        }
+    }
+}
