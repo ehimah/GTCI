f780306989d0d5c1cb653ee21c0986ba115b3d7b
Ehimah Obuse <ehimahobuse@gmail.com>
2020-07-26 12:56:29 +0100

    Update 01 - RemoveDuplicates.py

--- 02. Two Pointers/01 - RemoveDuplicates.py
+++ 02. Two Pointers/01 - RemoveDuplicates.py
@@ -1,4 +1,4 @@
-
+# coding: utf-8
 """ 
 Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space; after removing the duplicates in-place return the new length of the array.
 
@@ -24,9 +24,36 @@ def remove_duplicates(arr):
 
     return nextNonDuplicateIndex
 
+
+"""  
+Similar Questions #
+Problem 1: Given an unsorted array of numbers and a target ‘key’, remove all instances of ‘key’ in-place and return the new length of the array.
+
+Example 1:
+
+Input: [3, 2, 3, 6, 3, 10, 9, 3], Key=3
+Output: 4
+Explanation: The first four elements after removing every 'Key' will be [2, 6, 10, 9].
+
+Example 2:
+
+Input: [2, 11, 2, 2, 1], Key=2
+Output: 2
+Explanation: The first two elements after removing every 'Key' will be [11, 1].
+"""
     
+def remove_all_instances_of_element(arr, key):
+    arraySize=0
+    for i in range(len(arr)):
+        if arr[i] != key:
+            arr[arraySize]=arr[i]
+            arraySize += 1
+    return arraySize
+
+
 def main():
     print remove_duplicates([2,2, 3, 3, 3, 6, 9, 9])
     print remove_duplicates([2, 2, 2, 11])
-
+    print remove_all_instances_of_element([3, 2, 3, 6, 3, 10, 9, 3], 3)
+    print remove_all_instances_of_element([2, 11, 2, 2, 1], 2)
 main()
