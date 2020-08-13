6418f7b776eab66cf311a4910466c4b467f6cde3
Ehimah Obuse <ehimahobuse@gmail.com>
2020-08-13 02:11:18 +0100

    Create 00 - ReverseLinkedList.py

---
+++ 06. In Place Reversal of LinkedList/00 - ReverseLinkedList.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
+
+
+def reverse(head):
+    previous = None
+    while head is not None:
+        temp = head.next # temporarily store the next node
+        head.next = previous # reverse the current node
+        previous = head # before we move to the next node, point previous to the current node
+        head = temp
+    return previous
+
+
+def main():
+    head = Node(2)
+    head.next = Node(4)
+    head.next.next = Node(6)
+    head.next.next.next = Node(8)
+    head.next.next.next.next = Node(10)
+
+    print("Nodes of original LinkedList are: ", end='')
+    head.print_list()
+    result = reverse(head)
+    print("Nodes of reversed LinkedList are: ", end='')
+    result.print_list()
+
+
+main()
