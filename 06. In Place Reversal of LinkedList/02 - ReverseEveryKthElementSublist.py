b9833ed7737a96d85b7e34890f8a59b4be45d3b3
Ehimah Obuse <ehimahobuse@gmail.com>
2020-09-05 14:19:17 +0100

    Create 02 - ReverseEveryKthElementSublist.py

---
+++ 06. In Place Reversal of LinkedList/02 - ReverseEveryKthElementSublist.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+
+
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+  def print_list(self):
+    temp = self
+    while temp is not None:
+      print(temp.value, end=" ")
+      temp = temp.next
+    print()
+
+
+def reverse_every_k_elements(head, k):
+  # TODO: Write your code here
+  return head
+
+
+def main():
+  head = Node(1)
+  head.next = Node(2)
+  head.next.next = Node(3)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(5)
+  head.next.next.next.next.next = Node(6)
+  head.next.next.next.next.next.next = Node(7)
+  head.next.next.next.next.next.next.next = Node(8)
+
+  print("Nodes of original LinkedList are: ", end='')
+  head.print_list()
+  result = reverse_every_k_elements(head, 3)
+  print("Nodes of reversed LinkedList are: ", end='')
+  result.print_list()
+
+
+main()
+
+
+
+
+
+
+
